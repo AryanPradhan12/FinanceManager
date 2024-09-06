608741534d2b01db710460b2db959927c985f160
AryanPradhan <Aryanrp2007@gmail.com>
2024-09-06 14:46:48 +0400

    Initial push

---
+++ FinanceManager/FinanceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagerApp.swift
+//  FinanceManager
+//
+//  Created by Aryan Pradhan on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinanceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
